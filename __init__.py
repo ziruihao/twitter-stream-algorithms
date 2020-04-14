779161b25212709dd97f5a17859d48b5244f8dff
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-14 19:58:14 -0400

    changed stream params

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from stream import Stream
 from algorithms.misra_gries import MisraGries
 import Levenshtein
 
-# print(Levenshtein.distance('AB', 'D'))
+# print(Levenshtein.distance('covid', 'COVID'))
 
 algo = MisraGries(10)
-s = Stream(500, algo)
+s = Stream(5000, algo)
 s.set_filter(['USA'])
 print(algo.get_all_data())
