5127443dcb4a67b57ed8f5e9b143c818e94ed7cd
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-14 19:47:32 -0400

    different configs for the stream

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from stream import Stream
 from algorithms.misra_gries import MisraGries
+import Levenshtein
+
+# print(Levenshtein.distance('AB', 'D'))
 
 algo = MisraGries(10)
-s = Stream(100, algo)
+s = Stream(500, algo)
 s.set_filter(['USA'])
 print(algo.get_all_data())
