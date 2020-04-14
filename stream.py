164615115108d2711972079c4fc2fc2b35e4bb1a
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-14 19:01:54 -0400

    modified parsing method for tweets

--- stream.py
+++ stream.py
@@ -1,10 +1,14 @@
 import dotenv
 import os
 import tweepy
+import json
 
 class Stream(tweepy.StreamListener):
-    def __init__(self):
+    def __init__(self, limit, algorithm):
         dotenv.load_dotenv()
+        self.limit = limit
+        self.counter = 0
+        self.algorithm = algorithm
         self.auth = tweepy.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_KEY_SECRET'))
         self.auth.set_access_token(os.getenv('TWITTER_TOKEN'), os.getenv('TWITTER_TOKEN_SECRET'))
         self.stream = tweepy.Stream(self.auth, self)
@@ -18,15 +22,15 @@ class Stream(tweepy.StreamListener):
     def on_status(self, status):
             print(status.text)
       
-    def on_data(self, data):  
-        print(data)
-        return True
+    def on_data(self, data):
+        if (self.counter >= self.limit): self.stream.disconnect()
+        else:
+            tweet = json.loads(data)
+            self.counter += 1
+            for hashtag in (tweet['entities']['hashtags']):
+                self.algorithm.process(hashtag['text'])
+            return True
 
     def on_error(self, status):
         print(status)
-
-
-print('hi')
-s = Stream()
-s.set_filter(['#covid-19'])
 
