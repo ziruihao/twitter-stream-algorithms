584da96d8f3f59575abd6d19e4c6ac711044b02c
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-14 19:01:37 -0400

    constructed abstract frequency estimation algorithm class

---
+++ algorithms/frequency_estimation_algorithm.py
@@ -0,0 +1,21 @@
+import abc
+
+class FrequencyEstimationAlgorithm(abc.ABC):
+    def __init__(self):
+        super().__init__()
+        self.est_freqs = {}
+        self.actual_freqs = {}
+        self.stream_length = 0
+    
+    @abc.abstractmethod
+    def initialize(self):
+        pass
+
+    @abc.abstractmethod
+    def process(self, token):
+        pass
+
+    @abc.abstractmethod
+    def query(self, query):
+        pass
+
