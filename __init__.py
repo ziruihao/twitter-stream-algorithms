eb2f7d3e44636f2b93b2a410ce85e848ebc08ca9
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-16 00:32:43 -0400

    config

--- __init__.py
+++ __init__.py
@@ -6,9 +6,11 @@ import re
 # print(Levenshtein.distance('covid', 'COVID'))
 
 algo = MisraGries(10)
-s = Stream(5000, algo)
+s = Stream(10, algo)
 s.set_mode('text')
 s.set_filter(['college'])
-print(algo.get_actual_freqs())
+
+a = list(dict.items(algo.get_actual_freqs())).sort(key=lambda e: e[1])
+print(a)
 print('estimate')
 print(algo.get_est_freqs())
