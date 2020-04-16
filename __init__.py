f93b1b968ea6566ef5d7c93422183908fa4d1a09
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-16 00:21:39 -0400

    config

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 from stream import Stream
 from algorithms.misra_gries import MisraGries
 import Levenshtein
+import re
 
 # print(Levenshtein.distance('covid', 'COVID'))
 
 algo = MisraGries(10)
 s = Stream(5000, algo)
-s.set_filter(['USA'])
-print(algo.get_all_data())
+s.set_mode('text')
+s.set_filter(['college'])
+print(algo.get_actual_freqs())
+print('estimate')
+print(algo.get_est_freqs())
