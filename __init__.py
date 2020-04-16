791775453f6ec21a63b06a45bfaf7080272ec924
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-16 12:30:48 -0400

    configs

--- __init__.py
+++ __init__.py
@@ -2,14 +2,16 @@ from stream import Stream
 from algorithms.misra_gries import MisraGries
 import Levenshtein
 import re
+import json
+
 
 algo = MisraGries()
 algo.initialize(10, 'sequence_match')
 
-s = Stream(10000, algo)
+s = Stream(10, algo)
 s.set_mode('text')
 s.set_filter(['college'])
-print('done')
+print('Done, exported to ' + str(hash(algo)))
 
 algo.get_est_freqs()
 algo.get_actual_freqs()
