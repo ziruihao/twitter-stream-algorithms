f0d55144cc6066281dc7f1e55310f8b1403c6963
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-16 23:52:12 -0400

    cleaned up Morris counter

--- algorithms/morris.py
+++ algorithms/morris.py
@@ -1,16 +1,21 @@
-import copy
 import json
 from random import randint
 from math import pow 
-from algorithms.counting_algorithm import CountingAlgorithm
+from algorithms.abstract_algorithm import AbstractAlgorithm
 
-class MorrisCounter(CountingAlgorithm):
+class MorrisCounter(AbstractAlgorithm):
     def __init__(self):
         super().__init__()
     
     def process(self, token):
-        if (randint(0, pow(2, self.count)) is 0):
+        if (randint(1, pow(2, self.count)) is 0):
             self.count += 1
 
-    def query(self):
+    def query(self, token):
+        pass
+
+    def query_all(self, id):
+        print('Morris exporting to ' + id)
+        with open('data/moris-' + id + '.json', 'w') as f:
+            json.dump({'length': (pow(2, self.count) - 1)}, f)
         return pow(2, self.count) - 1
