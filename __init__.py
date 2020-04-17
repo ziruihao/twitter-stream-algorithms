e3fde62a0e49c0c488fcce11d2bd9c590034b103
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-17 00:18:57 -0400

    added time stamp for data export naming

--- __init__.py
+++ __init__.py
@@ -1,17 +1,21 @@
 from stream import Stream
-from algorithms.misra_gries import MisraGries
-import Levenshtein
-import re
-import json
+from streaming_algorithms import MisraGries
+from streaming_algorithms import Exact
+from streaming_algorithms import MorrisCounter
+import time
 
+stamp = str(int(time.time()) % 10000)
 
-algo = MisraGries()
-algo.initialize(10, 'sequence_match')
+exact = Exact()
 
-s = Stream(10, algo)
+misra = MisraGries(10, 'sequence_match')
+
+morris = MorrisCounter()
+
+s = Stream(10, [misra, exact, morris])
 s.set_mode('text')
 s.set_filter(['college'])
-print('Done, exported to ' + str(hash(algo)))
 
-algo.get_est_freqs()
-algo.get_actual_freqs()
+misra.query_all(stamp)
+morris.query_all(stamp)
+exact.query_all(stamp)
