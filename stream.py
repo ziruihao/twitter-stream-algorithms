0aa490367fbd37af07e527c518fac2f5f9963061
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-16 23:51:41 -0400

    Stream class can now push stream data to multiple algorithms in tandem

--- stream.py
+++ stream.py
@@ -4,22 +4,12 @@ import tweepy
 import json
 import re
 
-def clean_twitter_text(text):
-    exclude_words = ['rt', ' ']
-    extracted_words = []
-    text = re.sub(r'[ ]{3}', '', text)
-    text = re.sub(r'((http:\/\/|https:\/\/)([\S]+))', '', text).replace('\n', ' ')
-    for word in re.findall(r"(?=\S*['-])?([a-zA-Z'-]+)", text):
-        if (word not in exclude_words and re.match(r'[^0-9]+', word)):
-            extracted_words.append(word.lower())
-    return extracted_words
-
 class Stream(tweepy.StreamListener):
-    def __init__(self, limit, algorithm):
+    def __init__(self, limit, algorithms):
         dotenv.load_dotenv()
         self.limit = limit
         self.counter = 0
-        self.algorithm = algorithm
+        self.algorithms = algorithms
         self.mode = 'hashtag'
         self.auth = tweepy.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_KEY_SECRET'))
         self.auth.set_access_token(os.getenv('TWITTER_TOKEN'), os.getenv('TWITTER_TOKEN_SECRET'))
@@ -45,14 +35,27 @@ class Stream(tweepy.StreamListener):
                 self.counter += 1
                 if (self.mode is 'hashtag'):
                     for hashtag in (tweet['entities']['hashtags']):
-                        self.algorithm.process(hashtag['text'].lower())
+                        for algorithm in self.algorithms:
+                            algorithm.process(hashtag['text'].lower())
                 elif (self.mode is 'text'):
-                    for word in clean_twitter_text(tweet['text']):
-                        self.algorithm.process(word)
+                    for word in self.clean_twitter_text(tweet['text']):
+                        for algorithm in self.algorithms:
+                            algorithm.process(word)
                 elif (self.mode is 'location'):
                     if (tweet['place']):
-                        self.algorithm.process(tweet['place']['name'])
+                        for algorithm in self.algorithms:
+                            algorithm.process(tweet['place']['name'])
             return True
 
     def on_error(self, status):
         print(status)
+
+    def clean_twitter_text(self, text):
+        exclude_words = ['rt', ' ']
+        extracted_words = []
+        text = re.sub(r'[ ]{3}', '', text)
+        text = re.sub(r'((http:\/\/|https:\/\/)([\S]+))', '', text).replace('\n', ' ')
+        for word in re.findall(r"(?=\S*['-])?([a-zA-Z'-]+)", text):
+            if (word not in exclude_words and re.match(r'[^0-9]+', word)):
+                extracted_words.append(word.lower())
+        return extracted_words
