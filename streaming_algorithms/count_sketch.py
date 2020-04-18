562e76a2d0f87be057af3d5a3d512d7826ca9783
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-18 19:48:48 -0400

    incorporated medians feature within count sketch

--- streaming_algorithms/count_sketch.py
+++ streaming_algorithms/count_sketch.py
@@ -1,6 +1,7 @@
 import copy
 import json
 from random import choice
+from statistics import median
 from difflib import SequenceMatcher
 import Levenshtein
 from streaming_algorithms.abstract_algorithm import AbstractStreamingAlgorithm
@@ -8,27 +9,37 @@ from hash import Two_Universal_Hash
 from word_to_number import WordToNumber
 
 class CountSketch(AbstractStreamingAlgorithm):
-    def __init__(self, k):
+    def __init__(self, k, t):
         super().__init__()
         self.k = k
-        self.freqs = [0] * k
+        self.t = t
+        self.freqs = [[0 for _ in range(k)] for _ in range(t)]
 
         self.word_to_number = WordToNumber()
         distinct_elements_upper_bound = self.word_to_number.set_method('sha')
 
         self.H = Two_Universal_Hash(self.k, distinct_elements_upper_bound)
-        self.h = self.H.pick_hash()
+        self.h = [self.H.pick_hash() for _ in range(t)]
         self.G = Two_Universal_Hash(2, distinct_elements_upper_bound)
-        self.g = lambda : choice([-1, 1])
+        self.g = [self.G.pick_hash() for _ in range(t)]
 
     def process(self, token):
         token = self.word_to_number.convert(token)
-        self.freqs[self.h(token)] += self.g()
+        for t in range(self.t):
+            delta = self.g[t](token)
+            if (delta is 0): delta = -1
+            else: delta = 1
+            self.freqs[t][self.h[t](token)] += delta
 
     def query(self, query):
-        return self.freqs[self.h(self.word_to_number.convert(query))]
+        query = self.word_to_number.convert(query)
+        to_compute_medians = []
+        for t in range(self.t):
+            to_compute_medians.append(abs(self.freqs[t][self.h[t](query)]))
+        return median(to_compute_medians)
 
     def query_all(self, id):
+        self.freqs = list(map(lambda l: list(map(abs, l)), self.freqs))
         print('Exporting CountSketch to ' + id)
         with open ('data/countsketch-' + id + '.json', 'w') as f:
             json.dump({'freqs': self.freqs}, f)
