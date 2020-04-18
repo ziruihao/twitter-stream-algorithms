76ffa892040ef615d76e381f4709ca45d1cdcd4a
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-18 15:05:35 -0400

    fleshed out hashing in count sketch

--- streaming_algorithms/count_sketch.py
+++ streaming_algorithms/count_sketch.py
@@ -1,63 +1,35 @@
 import copy
 import json
+from random import choice
 from difflib import SequenceMatcher
 import Levenshtein
 from streaming_algorithms.abstract_algorithm import AbstractStreamingAlgorithm
+from hash import Two_Universal_Hash
+from word_to_number import WordToNumber
 
 class CountSketch(AbstractStreamingAlgorithm):
-    def __init__(self, k, scoring_method):
+    def __init__(self, k):
         super().__init__()
         self.k = k
+        self.freqs = [0] * k
 
-        # determining scoring method: https://stackoverflow.com/questions/6690739/high-performance-fuzzy-string-comparison-in-python-use-levenshtein-or-difflib
-        self.scorer = None
-        if (scoring_method is 'levenshtein'):
-            self.scorer = Levenshtein.ratio
-        elif (scoring_method is 'sequence_match'):
-            self.scorer = lambda t, e: SequenceMatcher(None, t, e).ratio()
+        self.word_to_number = WordToNumber()
+        distinct_elements_upper_bound = self.word_to_number.set_method('sha')
+
+        self.H = Two_Universal_Hash(self.k, distinct_elements_upper_bound)
+        self.h = self.H.pick_hash()
+        self.G = Two_Universal_Hash(2, distinct_elements_upper_bound)
+        self.g = lambda : choice([-1, 1])
 
     def process(self, token):
-        print(token)
-
-        # if we are already logging this token
-        best_match = self.found_in(token, self.freqs.keys(), self.scorer)
-        if (best_match is not ''):
-            self.freqs[best_match] += 1
-            
-        else:
-            # if there is still space to log this token
-            if (len(self.freqs.keys()) < self.k):
-                self.freqs[token] = 1
-            else:
-                # decrement all freqs
-                copy_of_keys = copy.copy(list(self.freqs.keys()))
-                for key in copy_of_keys:
-                    self.freqs[key] += -1
-                    if (self.freqs[key] is 0):
-                        self.freqs.pop(key)
+        token = self.word_to_number.convert(token)
+        self.freqs[self.h(token)] += self.g()
 
     def query(self, query):
-        if (query in self.freqs.keys()):
-            return self.freqs[query]
-        else:
-            return 'Too infrequent to provide estimate'
+        return self.freqs[self.h(self.word_to_number.convert(query))]
 
     def query_all(self, id):
         print('Exporting CountSketch to ' + id)
         with open ('data/countsketch-' + id + '.json', 'w') as f:
             json.dump({'freqs': self.freqs}, f)
         return self.freqs
-
-    # this method will return either itself or a word sufficiently 'close' to it from the list
-    def found_in(self, token, list, scorer):
-        if (len(list) is 0): return ''
-        else:
-            distances = []
-            for element in list:
-                # print({'token': token, 'element': element, 'distance': scorer(token, element)})
-                distances.append({'element': element, 'distance': scorer(token, element)})
-            distances.sort(key=lambda e: e['distance'])
-            if (distances[0]['distance'] > 0.8):
-                print('Substituting the word "' + token + '" for "' + distances[0]['element'] + '"')
-                return distances[0]['element']
-            else: return ''
