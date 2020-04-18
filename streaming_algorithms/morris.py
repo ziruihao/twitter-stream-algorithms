81c644dc542cb774f5389f12982e89d9154b4a5f
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-18 19:59:53 -0400

    added medians to morris

--- streaming_algorithms/morris.py
+++ streaming_algorithms/morris.py
@@ -1,20 +1,27 @@
 import json
 from random import randint
+from statistics import median
 from streaming_algorithms.abstract_algorithm import AbstractStreamingAlgorithm
 
 class MorrisCounter(AbstractStreamingAlgorithm):
-    def __init__(self):
+    def __init__(self, t):
         super().__init__()
+        self.t = t
+        self.count = [0 for _ in range(t)]
     
     def process(self, token):
-        if (randint(1, 2**self.count) is 1):
-            self.count += 1
+        for t in range(self.t):
+            if (randint(1, 2**(self.count[t])) is 1):
+                self.count[t] += 1
 
     def query(self, token):
         pass
 
     def query_all(self, id):
+        to_compute_medians = []
+        for t in range(self.t):
+            to_compute_medians.append(2**self.count[t] - 1)
         print('Morris exporting to ' + id)
         with open('data/moris-' + id + '.json', 'w') as f:
-            json.dump({'length': (2**self.count - 1)}, f)
-        return 2**self.count - 1
+            json.dump({'length': median(to_compute_medians)}, f)
+        return median(to_compute_medians)
