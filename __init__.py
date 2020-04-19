8895643179f1239f34dc6829d321cb6528f93e08
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-18 20:02:57 -0400

    package update

--- __init__.py
+++ __init__.py
@@ -2,20 +2,30 @@ from stream import Stream
 from streaming_algorithms import MisraGries
 from streaming_algorithms import Exact
 from streaming_algorithms import MorrisCounter
+from streaming_algorithms import CountSketch
+from streaming_algorithms import BJKST
 import time
+
+from word_to_number import WordToNumber
 
 stamp = str(int(time.time()) % 10000)
 
 exact = Exact()
 
-misra = MisraGries(10, 'sequence_match')
+misra = MisraGries(k=10, scoring_method='sequence_match')
 
-morris = MorrisCounter()
+morris = MorrisCounter(t=500)
 
-s = Stream(10, [misra, exact, morris])
+count_sketch = CountSketch(k=10, t=500)
+
+bjkst = BJKST(k=10, t=500)
+
+s = Stream(10, [misra, exact, morris, count_sketch, bjkst])
 s.set_mode('text')
 s.set_filter(['college'])
 
 misra.query_all(stamp)
 morris.query_all(stamp)
 exact.query_all(stamp)
+count_sketch.query_all(stamp)
+bjkst.query_all(stamp)
