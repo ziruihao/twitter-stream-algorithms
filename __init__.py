d1ef10db40cca645d93dca83cf74d068c385fc52
ziruihao <ziray.hao.22@dartmouth.edu>
2020-04-19 00:04:24 -0400

    config

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from stream import Stream
+from twitter_stream import TwitterStream
+from shakespeare_stream import ShakespeareStream
 from streaming_algorithms import MisraGries
 from streaming_algorithms import Exact
 from streaming_algorithms import MorrisCounter
@@ -8,22 +9,26 @@ import time
 
 stamp = str(int(time.time()) % 10000)
 
-exact = Exact()
+algorithms = []
 
-misra = MisraGries(k=25, scoring_method='sequence_match')
+algorithms.append(Exact())
+algorithms.append(MisraGries(k=25, scoring_method='sequence_match'))
+algorithms.append(MorrisCounter(t=500))
+algorithms.append(CountSketch(k=100, t=500))
+algorithms.append(BJKST(k=10, t=500))
 
-morris = MorrisCounter(t=500)
+# twitter_stream = TwitterStream(1000, algorithms)
+# twitter_stream.set_mode('text')
+# twitter_stream.set_filter(['college'])
 
-count_sketch = CountSketch(k=100, t=500)
+shakespeare_stream = ShakespeareStream(algorithms)
+shakespeare_stream.stream('shakespeare', 6)
 
-bjkst = BJKST(k=10, t=500)
+for algorithm in algorithms:
+    algorithm.query_all(stamp)
 
-s = Stream(1000, [misra, exact, morris, count_sketch, bjkst])
-s.set_mode('text')
-s.set_filter(['college'])
-
-misra.query_all(stamp)
-morris.query_all(stamp)
-exact.query_all(stamp)
-count_sketch.query_all(stamp)
-bjkst.query_all(stamp)
+# misra.query_all(stamp)
+# morris.query_all(stamp)
+# exact.query_all(stamp)
+# count_sketch.query_all(stamp)
+# bjkst.query_all(stamp)
